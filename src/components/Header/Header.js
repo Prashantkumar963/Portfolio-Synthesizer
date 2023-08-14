01b9673c4544a59c463164dfd65453b06455ea7e
Prashantkumar963 <prashantkumar.p20@iiits.in>
2023-08-14 13:21:57 +0530

    demo1

---
+++ src/components/Header/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import resumeSvg from "../../assets/resume.svg";
+
+import styles from "./Header.module.css";
+
+function Header() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <p className={styles.heading}>
+          <span>Portfolio-Synthesizer</span>
+          <br />A Portfolio that stands out!
+        </p>
+        <p className={styles.heading}>
+         <span>It's free</span>
+        </p>
+      </div>
+      <div className={styles.right}>
+        <img src={resumeSvg} alt="Resume" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
